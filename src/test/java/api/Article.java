ded23e0a5e30c914d3e473c2b7de9a594fa85161
Eregor <vints.diamond@gmail.com>
2024-05-20 21:49:35 +0300

    залит проект

---
+++ src/test/java/api/Article.java
@@ -0,0 +1,27 @@
+
+package api;
+
+import java.util.List;
+
+import io.qameta.allure.internal.shadowed.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Article {
+
+
+    private List<Object> categories;
+    private String content;
+    private String lang;
+    private String layout;
+    private List<Object> tags;
+    private String title;
+    private String url;
+
+
+}
---
+++ src/test/java/config/TestConfig.java
@@ -0,0 +1,11 @@
+package config;
+
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+
+
+@Getter
+@Log4j2
+public class TestConfig {
+    String URL = "https://www.healthcare.gov/api/";
+}
